74424317ab25c56a0d3467538794f545b9417ed9
davtir <davide.tiriticco@gmail.com>
2016-04-30 12:07:43 +0200

    Pe forza...

--- app/src/main/java/com/example/pervsive/testapplication/MainActivity.java
+++ app/src/main/java/com/example/pervsive/testapplication/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.pervsive.testapplication;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = (Button)findViewById(R.id.search_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+            }
+        });
     }
 }
